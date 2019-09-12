d039d8a0cf6c839c0d6da92206b5820efd89b3e7
das <ardynn.d@gmail.com>
2019-09-12 21:33:54 +0700

    like unlike delete

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,10 +1,19 @@
-//sementara
-function authorization ( req, res ,next ) {
-    if( req.decode ){
-        next()
-    } else {
-        next({ status : 401 , message : "Not Authorized" })
-    }
-}
+const Music = require('../models/music');
 
-module.exports = authorization;
+module.exports = {
+
+    AuthOwner(req, res, next) {
+        const { id } = req.decode;
+        const _id = req.params.id;
+        Music.findById(_id)
+            .then(music => {
+                if (music.UserId == id) {
+                    next();
+                } else {
+                    next({ status: 403, message: `You dont have access to delete this music` });
+                }
+            })
+            .catch(next)
+    },
+
+};
