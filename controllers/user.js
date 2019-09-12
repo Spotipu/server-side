2d4158bbb0393972a91fbda15e24a0ff4a163caf
Ricardo Tan <ricardo.tan.2000@gmail.com>
2019-09-12 16:47:34 +0700

    fitur login register selesai

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,34 @@
-const User= require('../models/user')
+const User = require('../models/user')
+const { compare } = require('../helpers/bcrypt')
+const { generateToken } = require('../helpers/jwt');
 class UserController {
     static login ( req , res ,next ){
-
+        const { email , password } = req.body;
+        User.findOne({ email })
+        .then ( found => {
+            if ( found ) {
+                if ( compare( password , found.password ) ) {
+                    const token = generateToken({
+                        id : found._id,
+                        email : found.email
+                    })
+                    res.status(200).json({ status: 200 , token })
+                } else {
+                    next({ status : 400 , message : "Invalid Email/Password"})
+                }
+            } else {
+                next({ status : 400 , message : "Invalid Email/Password"})
+            }
+        })
+        .catch( next )
     }
     static register ( req, res ,next ) {
-        
+        const { email, password } = req.body;
+        User.create({ email , password })
+        .then ( createdUser => {
+            res.status(200).json({ status: 200 , createdUser });
+        })
+        .catch( next );
     }
 }
 
