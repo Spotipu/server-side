99250b166f3c806ef7dc708a51ac9bb362bce127
Ricardo Tan <ricardo.tan.2000@gmail.com>
2019-09-12 15:59:38 +0700

    add initial register

---
+++ controllers/user.js
@@ -0,0 +1,11 @@
+const User= require('../models/user')
+class UserController {
+    static login ( req , res ,next ){
+
+    }
+    static register ( req, res ,next ) {
+        
+    }
+}
+
+module.exports = UserController;
