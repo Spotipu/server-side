107730d4435d3e81e9379ee0639702ac4550d8c2
das <ardynn.d@gmail.com>
2019-09-12 20:13:57 +0700

    add upload music

---
+++ helpers/music.js
@@ -0,0 +1,60 @@
+'use strict'
+require('dotenv').config();
+const path = require('path')
+const { Storage } = require('@google-cloud/storage');
+const CLOUD_BUCKET = process.env.CLOUD_BUCKET;
+
+const storage = new Storage({
+    projectId: process.env.CLOUD_PROJECT,
+    keyFilename: process.env.KEYFILE_PATH
+});
+const bucket = storage.bucket(CLOUD_BUCKET)
+const url = (filename) => {
+    return `https://storage.googleapis.com/${CLOUD_BUCKET}/${filename}`
+};
+const sendUploadToGCS = (req, res, next) => {
+    if (!req.file) {
+        return next();
+    }
+    const gcsname = Date.now() + req.file.originalname;
+    const file = bucket.file(gcsname);
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
+        }
+    });
+    stream.on('error', (err) => {
+        console.log('test error')
+        req.file.cloudStorageError = err
+        next(err);
+    });
+    stream.on('finish', () => {
+        console.log('test')
+        req.file.cloudStorageObject = gcsname
+        file.makePublic().then(() => {
+            req.file.url = url(gcsname)
+            next();
+        });
+    });
+    stream.end(req.file.buffer);
+}
+const Multer = require('multer'),
+    multer = Multer({
+        fileFilter: function (req, file, next) {
+            if ((path.extname(file.originalname) === '.mp3') || (path.extname(file.originalname) === '.wav') || (path.extname(file.originalname) === '.m4a') || (path.extname(file.originalname) === '.aac')) {
+                next(null, true);
+            } else {
+                next(err);
+            }
+        },
+        storage: Multer.MemoryStorage,
+        limits: {
+            fileSize: 10485760
+        }
+    });
+
+module.exports = {
+    url,
+    sendUploadToGCS,
+    multer
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const musicRouter =  require('./music');
-const musicRouter =  require('./user');
+// const musicRouter =  require('./user');
 
-
-router.use('/user' , userRouter );
+// router.use('/user' , userRouter );
 router.use('/music' , musicRouter );
-
 
 module.exports = router;
